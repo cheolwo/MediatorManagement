2e53116ba28c3c50f3c6c5ff0c23c365d4021099
cheolwo <powerqkrcjfd@gmail.com>
2024-03-25 01:14:32 +0900

    .

---
+++ 중개관리자App/중개관리자App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace 중개관리자App.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ 중개관리자App/중개관리자App/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace 중개관리자App.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
