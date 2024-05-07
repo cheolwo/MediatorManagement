4265e59562d92c6f0ab78f6cbbbb1102b4f0cd43
cheolwo <powerqkrcjfd@gmail.com>
2024-05-07 17:56:06 +0900

    .

--- 중개Server/Program.cs
+++ 중개Server/Program.cs
@@ -1,19 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using 재무인사Common.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 데이터베이스 컨텍스트 및 데이터 시더 서비스 추가
+// 데이터베이스 컨텍스트를 환경에 따라 다르게 설정
+if (builder.Environment.IsDevelopment())
+{
+    // 개발 환경: InMemory 데이터베이스 사용
+    builder.Services.AddDbContext<재무인사DbContext>(options =>
+        options.UseInMemoryDatabase("InMemoryDb"));
+}
+else
+{
+    // 프로덕션 환경: SQL Server 데이터베이스 사용
+    builder.Services.AddDbContext<재무인사DbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
+builder.Services.AddScoped<DataSeeder>();
 
+// 기타 서비스 구성
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 데이터베이스 초기화를 위한 로직 추가
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    var seeder = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataSeeder>();
+    var filePath = Path.Combine(app.Environment.ContentRootPath, "Setup", "은행코드.xlsx");
+    seeder.SeedDataAsync(filePath).Wait();
 }
 
 app.UseHttpsRedirection();
--- 중개Server/obj/Debug/net8.0/중개Server.AssemblyInfo.cs
+++ 중개Server/obj/Debug/net8.0/중개Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("중개Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0bf216c72745a078370e6099759b1457f26e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("중개Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("중개Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
